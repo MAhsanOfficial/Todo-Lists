c32ca9dc480068a9c44c0c11f5273ee554106577
Muhammad Ahsan <idahsanali@gmail.com>
2024-03-30 23:57:17 -0700

    todo list with using array and inquirer

---
+++ main.ts
@@ -0,0 +1,25 @@
+import inquirer  from "inquirer";
+let todos = [];
+let condition:boolean = true;
+
+while(condition){
+let addTask = await inquirer.prompt([
+    {
+        name:"todo",
+        type:"input",
+        message:"What you Want to Add in your Todos ?",
+    },
+    {
+        message:"Do you want to Add more ?",
+        name:"addMore",
+        type:"confirm",
+        default:"false",
+    }
+
+
+]);
+
+todos.push(addTask.todo);
+condition = addTask.addMore
+console.log(todos);
+};
